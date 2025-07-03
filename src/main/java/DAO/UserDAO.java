379bc1818298b3d711323c6e833154beb6bbb5e5
AdelinePat <adeline.patenne@hotmail.fr>
2025-07-03 17:00:15 +0200

    feat: UserDAO databaseOpenConnexion and databaseCloseConnexion

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,14 +1,37 @@
 package DAO;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO {
-//    public static Connexion databaseOpenConnexion() {
-//        return new Connexion();
-//    }
+    protected static final Dotenv dotenv = Dotenv.load();
+    protected static final String URL = dotenv.get("URL");
+    protected static final String USER = dotenv.get("USER");
+    protected static final String PASSWORD = dotenv.get("PASSWORD");
+    private static Connection connection = null;
+
+    public static Connection databaseOpenConnexion() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Database connection opened.");
+        }
+        return connection;
+    }
 
     public static void databaseCloseConnexion() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Database connection closed.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void createUser(List<String> userRequest) {
